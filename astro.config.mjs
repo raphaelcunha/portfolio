25623dd3dbd30e1196894cb8d580ae9250dda2d6
Raphael Cunha <raphael.s.cunha@gmail.com>
2023-05-01 15:29:44 -0300

    feat: first cards

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    assets: true,
+  },
   integrations: [tailwind()],
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,66 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    container: {
+      center: true,
+      screens: {
+        lg: "1160px",
+        xl: "1160px",
+      },
+    },
+    extend: {
+      borderRadius: {
+        "4xl": "2rem", // 32px
+      },
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: {
+          DEFAULT: "white",
+        },
+        secondary: {
+          DEFAULT: "rgba(188, 188, 188, 0.7)",
+        },
+        tertiary: {
+          DEFAULT: "rgba(188, 188, 188, 0.5)",
+        },
+      },
+
+      // typograpgy plugin
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            "h1, h3, h4, h5": {
+              color: theme("colors.white"),
+              lineHeight: "2.5rem", // 40px;
+              fontWeight: "600",
+              letterSpacing: "111%",
+              margin: 0,
+              padding: 0,
+            },
+            h2: {
+              fontSize: "1.25rem", // 20px
+              lineHeight: "20px",
+              fontWeight: "600",
+              letterSpacing: "-0.02em",
+              margin: 0,
+              padding: 0,
+            },
+            h5: {
+              fontSize: "14px", // 20px
+              lineHeight: "2.5rem", // 40px;
+              fontWeight: "600",
+              letterSpacing: "0.03em",
+              color: "rgba(188, 188, 188, 0.7)",
+            },
+            color: theme("colors.white"),
+          },
+        },
+      }),
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
 };
