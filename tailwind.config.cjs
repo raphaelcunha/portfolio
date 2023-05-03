33a0a766f64ff657665515f615ca1b9d669c467a
Raphael Cunha <raphael.s.cunha@gmail.com>
2023-05-03 08:09:55 -0300

    fix: build dependencies

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,8 +6,11 @@ module.exports = {
     container: {
       center: true,
       screens: {
-        lg: "1160px",
-        xl: "1160px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        '2xl': "1280px",
       },
     },
     extend: {
