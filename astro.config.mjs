e199e1b0d7d1ac176fa264b4f1d969d937ad87f2
Raphael Cunha <raphael.s.cunha@gmail.com>
2023-05-06 17:58:23 -0300

    fix: add picture module astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     assets: true,
   },
-  integrations: [tailwind()],
+  integrations: [tailwind(), image()],
 });
