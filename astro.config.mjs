fc23c7ca8731efa838968fe1ec608113fef48ea1
Raphael Cunha <raphael.s.cunha@gmail.com>
2023-05-12 18:23:48 -0300

    feat: add vercel plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,4 +11,8 @@ export default defineConfig({
     assets: true,
   },
   integrations: [tailwind(), image(), react()],
+  output: "server",
+  adapter: vercel({
+    analytics: true,
+  }),
 });
